052f411da5a6dd4a877e349b09dc18d9f610d6c5
kaixin.lu <kaixin.lu@UN3AZDXA87DWDSM>
2018-08-22 12:03:55 +0800

    u

--- src/cn/zjyy/appsys/controller/Controller.java
+++ src/cn/zjyy/appsys/controller/Controller.java
@@ -2,10 +2,9 @@ package cn.zjyy.appsys.controller;
 
 public class Controller {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("++999999999999+++++++++");
-=======
+
 		System.out.println("+++++++++++++++++pppppppppppppppppp");
->>>>>>> branch 'master' of https://github.com/kaixin789/AppInfoSystem.git
 	}
 }
--- src/cn/zjyy/appsys/dao/kkk.java
+++ src/cn/zjyy/appsys/dao/kkk.java
@@ -4,7 +4,7 @@ public class kkk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("开心尼玛");
+		System.out.println("999");
 	}
 
 }
